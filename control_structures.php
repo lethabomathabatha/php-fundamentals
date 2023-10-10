19b74f68b35b082758a0bab7b1d95e0facdceffc
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-10-10 17:23:37 +0200

    :sparkles: Add conditional examples

--- control_structures.php
+++ control_structures.php
@@ -7,7 +7,56 @@
 <body>
     <!-- Start PHP code -->
     <?php
+    /* Conditionals:
+        - if
+        - else
+        - else if
+        - switch
+    */
+
+    // using if, else if and else
+    $age = 16;
+
+    if($age < 18) {
+        echo ("Sorry, you are too young.");
+    } else if ($age > 50) {
+        echo ("Sorry, you are too old.");
+    } else {
+        echo ("Welcome, you are old enough.");
+    }
+
+    // using switch statements
+    $role = "Visitor";
+
+    switch ($role) {
+        case 'Visitor':
+            echo ("Welcome, Visitor!");
+        break;
+        case 'Admin':
+            echo ("Welcome, Admin!");
+        break;
+        case 'Superadmin':
+            echo ("Welcome, Superadmin!");
+        break;
+        case 'Boss':
+            echo ("Welcome, Boss!");
+        break;
+        default:
+            echo ("User has no role.");
+    }
+
+
+    /* Loops:
+        - while
+        - do while
+        - for
+        - foreach
+    */
+
+    // while
     
+
+
     ?> 
     <!-- End PHP code -->
 </body>
