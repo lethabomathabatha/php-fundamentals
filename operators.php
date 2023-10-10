9a9727d07244c0f1031289a9bbe3a445ee99d23d
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-10-10 14:50:09 +0200

    :tada: Add operators notes

---
+++ operators.php
@@ -0,0 +1,29 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operators in PHP</title>
+</head>
+<body>
+    <!-- Start PHP code -->
+    <?php
+        // Arithmetic operators: +,-,*,/,%
+        // Assignment operators: =, +=, -=, *=, /=, %=, .=
+        // Comparison operators: ==, ===, !=, !==, >, <, >=, <=
+
+        /* Comparison operator - spaceship: <=>. This operator is only available in PHP 7.1 and above
+            If the value of the left operand is less than the value of the right operand, the spaceship operator returns -1
+            If the value of the left operand is greater than the value of the right operand, the spaceship operator returns 1
+            If the value of the left operand is equal to the value of the right operand, the spaceship operator returns 0
+            
+            e.g: $5 <=> $7 returns -1 if $5 is less than $7, 0 if $5 is equal to $7, and 1 if $5 is greater than $7. 
+            In this case, $5 is less than $7 and therefore the spaceship operator returns -1
+        */
+
+        // Logical operators: And, &&, Or, ||, !, Xor
+        // Increment and decrement operators: ++$x, --$x, $x++, $x--
+
+    ?>
+    <!-- End PHP code -->
+</body>
+</html>
