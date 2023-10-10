ac6b6d22c9807ebb0e27edee042bde9d1a8f5dcf
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-10-10 14:55:27 +0200

    :tada: Add control structures notes

---
+++ control_structures.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Control Structures</title>
+</head>
+<body>
+    <!-- Start PHP code -->
+    <?php
+    
+    ?> 
+    <!-- End PHP code -->
+</body>
+</html>
