7b27e0dcf10a57a9e6fca0d23663a5a3e62e2b44
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-10-11 18:56:49 +0200

    :clown_face: Add exercises

--- exercises.php
+++ exercises.php
@@ -7,7 +7,7 @@
 <body>
     <!-- Start PHP code -->
     <?php 
-    // Exercises on data types
+    // EXERCISES ON DATA TYPES
     // Exercise 1: create two variables ('x', 'y'), add them, and multiply the sum by 5. Assign the output to a new variable 'z'.
     $x = 6;
     $y = 5;
@@ -76,7 +76,72 @@
     */
     $weather = array("rain", "sunshine", "clouds", "hail", "sleet", "snow", "wind");
 
-    echo "<br> We've seen all kinds of weather this month. At the beginning of the month, we had " . $weather[5] . " and " . $weather[6] . ". Then came ". $weather[1] . " with a few " . $weather[2] . " and some " . $weather[0] . ". At least we didn't get any " . $weather[3] . " or " . $weather[4] .  "." 
+    echo "<br> We've seen all kinds of weather this month. At the beginning of the month, we had " . $weather[5] . " and " . $weather[6] . ". Then came ". $weather[1] . " with a few " . $weather[2] . " and some " . $weather[0] . ". At least we didn't get any " . $weather[3] . " or " . $weather[4] .  "." ;
+
+
+    // EXERCISES ON CONTROL STRUCTURES
+    // Exercise 1: create two variables with two integer numbers. Print out which one is the highest and which one is the lowest value.
+    $numOne = 54;
+    $numTwo = 22;
+
+    if ($numOne > $numTwo) {
+        print ("<br> The highest value is {$numOne} and the lowest value is {$numTwo}");
+
+    } else if ($numOne < $numTwo) {
+        print ("<br> The highest value is {$numTwo} and the lowest value is {$numOne}");
+    } else {
+        print "<br> The value is does not appear to be a valid integer.";
+    }
+
+
+    // Exercise 2: create a variable with a value in-between number 1 to 4 (reject any other value). Based on the value, print out "Spades", "Hearts", "Diamonds", "Clubs".
+    $card = 3;
+
+    switch ($card) {
+        case 1:
+            print "<br> Spades";
+        break;
+        case 2:
+            print "<br> Hearts";
+        break;
+        case 3:
+            print "<br> Diamonds";
+        break;
+        case 4:
+            print "<br> Clubs";
+        break;
+        default: 
+            print "<br> The card does not appear to be valid.";
+    }
+
+
+    /*
+        Exercise 3: 
+        The American grading system works with the letters A, B, C, D, F. C or higher means that you have passed the subject.
+        Create a variable that shows the amount of points a student scored and based on that number, print out the letter grade.
+        Also, print out if the student has passed or failed the subject.
+        - A = 90 - 100
+        - B = 80 - 89
+        - C = 70 - 79
+        - D = 60 - 69
+        - F = 0 - 59
+    */
+    $score = -15;
+
+    if ($score >= 90 && $score <= 100) {
+        print "<br> A -  Passed";
+    } else if ($score >= 80 && $score <= 89) {
+        print "<br> B - Passed";
+    } else if ($score >= 70 && $score <=79) {
+        print "<br> C - Passed";
+    } else if ($score >= 69 && $score <= 69) {
+        print "<br> D - Failed";
+    } else if ($score >= 0 && $score <= 59) {
+        print "<br> F - Failed";
+    } else {
+        print "<br> The score does not appear to be valid.";
+    }
+
 
     ?>
     <!-- End PHP code  -->
